254b464b354c4c8c1ad8f00b50f9128cef6c2159
AHYEON <yotu00@naver.com>
2023-06-22 22:02:27 +0900

    react-beautiful-dnd test

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
 import React from 'react';
-import { useRecoilState, useRecoilValue } from 'recoil';
-import { hourSelector, minuteState } from './atoms';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 function App() {
-  const [ minutes, setMinutes ] = useRecoilState(minuteState);
-  const [ hours, setHours ] = useRecoilState(hourSelector);
-  const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setMinutes(+event.currentTarget.value);
-  }
-  const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setHours(+event.currentTarget.value);
-  };
+  const onDragEnd = () => {};
   return (
-    <div>
-      <input value={minutes} onChange={onMinutesChange} type='number' placeholder='Minutes' />
-      <input value={hours} onChange={onHoursChange} type='number' placeholder='Hours' />
-    </div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId='one'>
+        {(magic) => (
+          <ul ref={magic.innerRef} {...magic.droppableProps}>
+            <Draggable draggableId='first' index={0}>
+              {(magic) => (
+                <li ref={magic.innerRef} {...magic.draggableProps}>
+                  <span {...magic.dragHandleProps}> ☝🏻 </span> One
+                </li>
+              )}
+            </Draggable>
+            <Draggable draggableId='second' index={1}>
+              {(magic) => (
+                <li ref={magic.innerRef} {...magic.draggableProps}>
+                  <span {...magic.dragHandleProps}> ✌🏻 </span> Two
+                </li>
+              )}
+            </Draggable>
+          </ul>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -72,10 +72,8 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <RecoilRoot>
-      <GlobalStyled />
-      <App />
-    </RecoilRoot>
-  </React.StrictMode>
+  <RecoilRoot>
+    <GlobalStyled />
+    <App />
+  </RecoilRoot>
 );
